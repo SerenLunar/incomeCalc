d124bf7da3cc43262ad536d2c3c9b32bc17295bf
Khadija Gurbanli <qurbanlixedice2@gmail.com>
2024-11-17 11:36:36 +0400

    income

---
+++ KOL2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KOL2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string gelir = richTextBoxGelir.Text;
+            string xerc = richTextBoxXerc.Text;
+            string budce = labelBudce.Text;
+            int gelirInt = Convert.ToInt32(gelir);
+            int xercInt = Convert.ToInt32(xerc);
+            int budceInt = Convert.ToInt32(budce);
+            budceInt = budceInt + (gelirInt - xercInt);
+            labelBudce.Text = budceInt.ToString();
+        }
+    }
+}
